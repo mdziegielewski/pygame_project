5fe8467ebfe9b03bccd4fc426673acce8e57dc9f
Maciej <m.j.dziegielewski@wp.pl>
2019-01-18 15:07:36 +0100

    Reading

--- editing_files.py
+++ editing_files.py
@@ -15,3 +15,23 @@ out.close()
 
 with open("firstFile.txt", "a") as out:     #out-variable
     out.write("\ntest with")                #not necessary to close()
+
+
+inputFile = open("firstFile.txt", "r")      #r - read
+                                            # not necessary to use "r"
+fileContent = inputFile.read()
+print(fileContent)
+inputFile.close()
+
+print("-----------------")
+inputFile = open("firstFile.txt", "r")
+for line in inputFile:
+    print(line) #end=""
+inputFile.close()
+
+
+print("-----------------")
+with open("firstFile.txt", "r") as inputFile:
+    line = inputFile.readline()
+    line = inputFile.readline()
+    print(line)
